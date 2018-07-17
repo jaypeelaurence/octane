8a58199d2fc2275b774b19b11aff9b371292a5ee
jcocjin <jaypee@adspark.ph>
2018-07-17 12:42:35 +0800

    added logger in senderId

--- app/Report.php
+++ app/Report.php
@@ -20,12 +20,11 @@ class Report extends Model
         $accountId = $this->query->table('esme_credential');
         $accountId->select('allowed_sender_ids');
         $accountId->where('esme_credential.id', $id);
+        $accountId->;limit(1000000);
         $accountId->get();
 
         if(count($accountId->get()) != 0){
-            $string = $accountId->get()[0]->allowed_sender_ids;
-
-            $senderId = explode("|", $string, -1);
+            $senderId = explode("|", $accountId->get()[0]->allowed_sender_ids, -1);
 
             return $senderId;
         }else{
