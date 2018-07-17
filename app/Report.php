540c3822b57656d88ecaedfc5212e6e01fbad4bb
jcocjin <jaypee@adspark.ph>
2018-07-17 13:08:25 +0800

    added logger in senderId

--- app/Report.php
+++ app/Report.php
@@ -25,7 +25,7 @@ class Report extends Model
         if(count($accountId->get()) != 0){
             $senderId = explode("|", $accountId->get()[0]->allowed_sender_ids, -1);
 
-            return array_slice($senderId,0,2000);
+            return array_slice($senderId,0,1000);
         }else{
             return redirect('error/100');
         }
